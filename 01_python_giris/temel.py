4f99a82d7de44c7eb15fc6051cafb15f1f5f5943
Kübra Gülmez <kkubragulmez@gmail.com>
2021-03-15 20:01:09 +0300

    first commit

---
+++ 01_python_giris/temel.py
@@ -0,0 +1,43 @@
+#region escape 
+""""""
+print("Ecodation\teğitim\tkurumları")
+print("ecodation\neğitim\nkurumları")
+print("merhaba \"ecodation\"")
+print("Hayatta","En","Hakiki", end=">")
+print("Mürşit", "İlimdir")
+print("\"Ege\'nin\"", "\"İncisi\"", "\"İzmir!\"", sep="&")
+print("Dünya\'nın", "En", "Güzel", "Şehri", "İstanbul\'dur", sep="*", end=".")
+
+#endregion
+#region veri tipleri
+"""
+print(87)  #integer
+print("elma") #string
+print(65.98) #float
+print(True) #boolean
+print(type("ecodation")) 
+"""
+#endregion
+
+#region aritmetik_operatorler
+"""
+print(12+3)
+print(17-9)
+print(15*7)
+print(56/9)
+print(45//2) #tam bölme
+print(98%7) #mod alma
+print(4**.5) #üs alma
+print(10*(4*5)/(3**4))
+print(3-5+7*9-(8%3))
+print((1-5)**(9%4)+(96//7)*(48/5))
+"""
+#endregion
+
+
+
+
+
+
+
+
