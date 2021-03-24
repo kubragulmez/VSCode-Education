ef28319abd9663da886bea6d760813f20f64cfc2
Kübra Gülmez <kkubragulmez@gmail.com>
2021-03-24 13:41:53 +0300

    24032021

---
+++ 02_karar_yapilari/0205_break_point.py
@@ -0,0 +1,9 @@
+  
+#region break-point
+kAdi = input('Kullanici Adi Giriniz: ')
+print("a")
+if kAdi!= "admin":
+    print(f"{kAdi} yetkisi ile admin paneline giremezsiniz...")
+    print("b")
+print("c")
+#endregion
